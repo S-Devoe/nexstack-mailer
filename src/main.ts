dcd934cdfb20ef27806d628c0752e0a543efdf12
S-Devoe <chelseg@gmail.com>
2023-11-20 20:59:00 +0100

    feat: added cors

--- src/main.ts
+++ src/main.ts
@@ -21,6 +21,13 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('/doc', app, document);
+  app.enableCors({
+    origin: [/\.*\.*$/],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    credentials: true,
+  });
 
   await app.listen(process.env.PORT ?? 5500);
 }
